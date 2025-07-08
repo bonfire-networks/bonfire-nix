8fdea98f2e4facba0f6c245af83c72b536f2aab9
Giacomo Leidi <goodoldpaul@autistici.org>
2025-07-08 11:58:50 +0200

    vm.nix: Add first iteration of Bonfire container.

--- vm.nix
+++ vm.nix
@@ -25,6 +25,39 @@
     # backend defaults to "podman"
     backend = "docker";
     containers = {
+      bonfire = {
+        image = "docker.io/bonfirenetworks/bonfire:1.0.0-rc.1.24-social-amd64";
+        # We connect everything to the host network,
+        # this way we can use Nix provides services
+        # such as Postgres.
+        networks = [ "host" ];
+        volumes = [ "/var/lib/bonfire/uploads:/opt/app/data/uploads" ];
+        environment = {
+          # DB settings
+          POSTGRES_DB = "bonfire";
+          POSTGRES_USER = "bonfire";
+          POSTGRES_HOST = "localhost";
+          # Mail settings
+          # MAIL_DOMAIN = "FQDN";
+          # MAIL_FROM = "name@FQDN";
+          # MAIL_BACKEND = "backend";
+          # MAIL_PORT = "465";
+          # MAIL_SSL = "true";
+          # Instance settings
+          SEARCH_MEILI_INSTANCE = "http://localhost:7700";
+          FLAVOUR = "social";
+          PORT = "4000";
+          SERVER_PORT = "4000";
+          PUBLIC_PORT = "443";
+          # HOSTNAME = "FQDN";
+          # Technical settings
+          SEEDS_USER = "root";
+          MIX_ENV = "prod";
+          PLUG_BACKEND = "bandit";
+          APP_NAME = "Bonfire";
+          ERLANG_COOKIE = "bonfire_cookie";
+        };
+      };
       meilisearch = {
         image = "docker.io/getmeili/meilisearch:v1.14";
         # We connect everything to the host network,
