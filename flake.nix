71db8dbdd954d0c59563265dfa674e1fb1a75106
themarkness <williamsmarka@gmail.com>
2025-07-30 12:32:46 +0100

    add support for nixos-anywhere to deploy nixos on Hetzner VM

--- flake.nix
+++ flake.nix
@@ -2,25 +2,27 @@
   description = "A simple NixOS flake for Hetzner Cloud server with Bonfire";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    bonfire-app.url = "github:bonfire-networks/bonfire-app/main"; # Or a specific tag/commit
+    bonfire-app.url = "github:bonfire-networks/bonfire-app/main";
     flake-utils.url = "github:numtide/flake-utils";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, bonfire-app, flake-utils, sops-nix, ... }@inputs: {
-      nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix # Your main configuration.nix
-          ./hardware-configuration.nix # Add other modules here if you have them, e.g., hardware-configuration.nix
-          sops-nix.nixosModules.sops
-        ];
-        specialArgs = {
-          inherit bonfire-app; # Pass bonfire-app input to configuration.nix
-        };
+  outputs = { self, nixpkgs, bonfire-app, flake-utils, sops-nix, disko, ... }@inputs: {
+    nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        disko.nixosModules.disko
+        ./disko.nix
+        sops-nix.nixosModules.sops
+      ];
+      specialArgs = {
+        inherit bonfire-app;
       };
     };
+  };
 }
