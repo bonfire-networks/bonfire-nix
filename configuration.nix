bc4c303a8cef9b49fc29584191d8334df1f0b905
themarkness <williamsmarka@gmail.com>
2025-08-04 15:05:56 +0100

    update readme

--- configuration.nix
+++ configuration.nix
@@ -27,13 +27,13 @@
   sops.defaultSopsFile = ./secrets/hetzner.yaml;
 
   # This will automatically import SSH keys as age keys
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+ # sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   # This is using an age key that is expected to already be in the filesystem
   sops.age.keyFile = "/root/.config/sops/age/keys.txt";
 
   # This will generate a new key if the key specified above does not exist
-  sops.age.generateKey = false;
+  sops.age.generateKey = true;
 
   # This is the actual specification of the secrets.
   sops.secrets.meilisearch = {};
@@ -122,7 +122,7 @@
     vim
   ];
 
-  services.openssh = {
+  services.openssh.settings = {
     enable = true;
     permitRootLogin = "yes";
     passwordAuthentication = false;
